ee846e7a2ed261ae6a9d6d91012beaf41a2a1b7d
Will <will_leighton@outlook.com>
2021-02-09 19:02:51 +0000

    migrations untested

---
+++ database/migrations/2021_02_09_184229_create_actions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actions', function (Blueprint $table) {
+            $table->id();
+            $table->float('distanceKM', 8, 2);
+            $table->date('date_completed');
+            $table->timestamps();
+            
+            $table->foreignId('user_id')->unsigned();
+            $table->foreignId('activity_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('activity_id')->references('id')->on('activities')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actions');
+    }
+}
